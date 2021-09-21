c00171b748728c4349fc40cd3f2a59a49569064f
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2021-09-21 00:39:30 -0700

    Added SearchActivity feature

--- app/src/main/java/com/example/photogalleryapp/MainActivity.java
+++ app/src/main/java/com/example/photogalleryapp/MainActivity.java
@@ -16,12 +16,14 @@ import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int SEARCH_ACTIVITY_REQUEST_CODE = 2;
     String mCurrentPhotoPath;
     private ArrayList<String> photos = null;
     private int index = 0;
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        photos = findPhotos();
+        photos = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");
         if (photos.size() == 0){
             displayPhoto(null);
         } else {
@@ -41,20 +43,46 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SEARCH_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                DateFormat format = new SimpleDateFormat("yyyy‐MM‐dd HH:mm:ss");
+                Date startTimestamp , endTimestamp;
+                try {
+                    String from = (String) data.getStringExtra("STARTTIMESTAMP");
+                    String to = (String) data.getStringExtra("ENDTIMESTAMP");
+                    startTimestamp = format.parse(from);
+                    endTimestamp = format.parse(to);
+                } catch (Exception ex) {
+                    startTimestamp = null;
+                    endTimestamp = null;
+                }
+                String keywords = (String) data.getStringExtra("KEYWORDS");
+                index = 0;
+                photos = findPhotos(startTimestamp, endTimestamp, keywords);
+                if (photos.size() == 0) {
+                    displayPhoto(null);
+                } else {
+                    displayPhoto(photos.get(index));
+                }
+            }
+        }
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            ImageView mImageView = findViewById(R.id.ivGallery);
+            ImageView mImageView = (ImageView) findViewById(R.id.ivGallery);
             mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
-            photos = findPhotos();
+            photos = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");
         }
     }
 
-    private ArrayList<String> findPhotos(){
+    private ArrayList<String> findPhotos(Date startTimestamp, Date endTimestamp, String keywords){
         File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "/Android/data/com.example.photogalleryapp/files/Pictures");
         ArrayList<String> photos = new ArrayList<>();
         File[] fList = file.listFiles();
         if(fList != null){
-            for (File f : fList){
-                photos.add(f.getPath());
+            for (File f : fList) {
+                if (((startTimestamp == null && endTimestamp == null) || (f.lastModified() >= startTimestamp.getTime()
+                        && f.lastModified() <= endTimestamp.getTime())) && (keywords.equals("") || f.getPath().contains(keywords))) {
+                    photos.add(f.getPath());
+                }
             }
         }
         return photos;
@@ -90,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
     private void updatePhoto(String path, String caption) {
         String[] attr = path.split("_");
         if (attr.length >= 3) {
-            File to = new File(attr[0] + "_" + caption + "_" + attr[2] + "_" + attr[3]);
+            File to = new File(attr[0] + "_" + caption + "_" + attr[2] + "_" + attr[3] +"_");
             File from = new File(path);
             from.renameTo(to);
         }
@@ -123,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.btnNext:
-                if(index < (photos.size() -1)) {
+                if(index < (photos.size() - 1)) {
                     index++;
                 }
                 break;
@@ -133,4 +161,8 @@ public class MainActivity extends AppCompatActivity {
         displayPhoto(photos.get(index));
     }
 
+    public void searchButton(View v){
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivityForResult(intent, SEARCH_ACTIVITY_REQUEST_CODE);
+    }
 }
